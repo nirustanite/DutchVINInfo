c153a827b6c03f563d456f5d75d1d266c5ab64bf
Nirupamaa Jaya <nirustanite@gmail.com>
2021-02-05 10:40:00 +0100

    design

--- dutchvininfo/src/index.js
+++ dutchvininfo/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
         <Provider store={store}>
--- dutchvininfo/src/pages/HomePage/index.jsx
+++ dutchvininfo/src/pages/HomePage/index.jsx
@@ -1,9 +1,99 @@
 import React from 'react';
+import styled from 'styled-components';
+import image from '../../images/blackcar.jpeg';
+import SimpleCarousal from '../../components/SimpleCarousal';
 
+
+const ImageDiv = styled.div`
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    height: 300px;
+    text-align: center;
+    opacity: 0.5;
+
+    @media (max-width: 768px) {
+        height: 250px;
+    } 
+`;
+
+const InnerDiv = styled.div`
+    position: absolute;
+    top: 6%;
+    left: 35%;
+    margin: 0;
+    padding: 0;
+
+    @media (max-width: 768px) {
+       left: 5%;
+       left: 10%;
+       right: 10%;
+       text-align: center;
+    }
+`
+const StyledHeader = styled.h1`
+    color: white;
+
+    @media (max-width: 768px) {
+        font-size: 20px;
+     }
+`
+
+const DetailsDiv = styled.div`
+    position: relative;
+    background-color: white;
+    width: 400px;
+    height: 200px;
+    text-align: center;
+    border-radius: 25px;
+    box-shadow: 2px 2px 2px #D3D3D3;
+    background-clip: content-box;
+    margin: auto;
+    margin-top: -104px;
+    z-index: 4;
+
+    @media (max-width: 768px) {
+        margin-top: -98px;
+        width: 250px;
+        height: 150px;
+    }
+`
 const HomePage = () => {
     return(
         <React.Fragment>
-            Welcome
+            <ImageDiv 
+                style={{ 
+                    backgroundImage: `url(${image})`,
+                }}
+            > 
+                
+            </ImageDiv>
+
+            <InnerDiv>
+                <div style={{ display: 'flex' , flexDirection: 'column'}}>
+                    <StyledHeader>Please enter your license plate number</StyledHeader>
+                    <div style={{ textAlign: 'center' }}>
+                        <input type="text"/>
+                        <button>Send</button>
+                    </div>
+                </div>
+                   
+                    
+            </InnerDiv>  
+            <DetailsDiv>
+                wel
+            </DetailsDiv>
+            <div>
+                <section
+                    style={{
+                        margin: "20px 0 20px 0"
+                    }}
+                >
+                    <SimpleCarousal deviceType="desktop"/>
+                </section>
+            </div>
+  );
+           
         </React.Fragment>
     );
 };
